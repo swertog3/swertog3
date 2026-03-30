658dc303735343da7691a343c5ca1c0c70e1b41e
swertog3 <swertog@gmail.com>
2026-03-30 10:04:55 +0300

    Update form.php

--- 3/form.php
+++ 3/form.php
@@ -11,6 +11,39 @@
     }
     ?>
   </select>
-  
+  <label for="number">Телефон</label>
+  <input name="number" />
+  <label for="year">Дата рождения:</label>
+  <input name="year" />
+  <label>Пол</label>
+        <div>
+          <input type="radio" id="cFiveOne" name="gender" value="cFiveOne" required> 
+          <label for="cFiveOne">Муж</label>
+        </div>
+        <div>
+          <input type="radio" id="cFiveThree" name="gender" value="cFiveThree"> 
+          <label for="cFiveThree">Жен</label>
+        </div>
+        <div>
+          <input type="radio" id="cFiveFour" name="gender" value="cFiveFour"> 
+          <label for="cFiveFour">Не указан</label>
+        </div>
+      </div>
+  <label>Любимый язык программирования</label>
+        <select name="proga[]" multiple size="3"> 
+          <option value="Pascal">Pascal</option> 
+          <option value="C">C</option>
+          <option value="Cpp">C++</option>
+          <option value="JavaScript">JavaScript</option>
+          <option value="PHP">PHP</option>
+          <option value="Python">Python</option>
+          <option value="Java">Java</option>
+          <option value="Haskell">Haskell</option>
+          <option value="Clojure">Clojure</option>
+          <option value="Prolog">Prolog</option>
+          <option value="Scala">Scala</option>
+        </select>
+  <label for="biography">Биография</label>
+  <input name="biography" />
   <input type="submit" value="ok" />
 </form>
