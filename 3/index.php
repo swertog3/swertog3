8dd4e8c2bc641dadccb20ea319ce6b442158c53b
swertog3 <swertog@gmail.com>
2026-04-20 10:02:26 +0300

    Update index.php

--- 3/index.php
+++ 3/index.php
@@ -20,7 +20,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 
 // Проверяем ошибки.
 $errors = FALSE;
-if (empty($_POST['fio'])  || preg_match('/^\d+$/', $_POST['year']) ) {
+if (empty($_POST['fio'])  || preg_match('/^\d+$/', $_POST['year']) substr($_POST['year'],0,10) ) {
   print('Заполните имя.<br/>');
   $errors = TRUE;
 }
