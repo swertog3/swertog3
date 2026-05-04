3c4e398f4c443441ee664d42bbbf18f415faf016
swertog3 <swertog@gmail.com>
2026-05-04 10:28:31 +0300

    Update index.php

--- 3/index.php
+++ 3/index.php
@@ -31,7 +31,7 @@ if (empty($_POST['year']) || !is_numeric($_POST['year']) ) {
   $errors = TRUE;
 }
 
-if (empty($_POST['number']) || !preg_match('/^\+?[1-9]\d{1,14}$/')) {
+if (empty($_POST['number']) || !preg_match('/^\+?[1-9]\d{1,14}$/', $_POST['number'])) {
   print('Заполните телефон.<br/>');
   $errors = TRUE;
 }
