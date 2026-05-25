2b57a94bc668c801a84928c6f7ef9f3eb29ad5b7
swertog3 <swertog@gmail.com>
2026-05-26 02:15:33 +0300

    Update functions.php

--- 8/includes/functions.php
+++ 8/includes/functions.php
@@ -154,11 +154,15 @@ function generateCredentials($fullname, $email) {
 }
 
 function transliterate($text) {
-    $cyr = ['а','б','в','г','д','е','ё','ж','з','и','й','к','л','м','н','о','п','р','с','т','у','ф','х','ц','ч','ш','щ','ъ','ы','ь','э','ю','я'];
-    $lat = ['a','b','v','g','d','e','yo','zh','z','i','y','k','l','m','n','o','p','r','s','t','u','f','kh','ts','ch','sh','sch','','y','','e','yu','ya'];
-    return str_replace($cyr, $lat, mb_strtolower($text));
+    // Транслитерация без mbstring
+    $cyr = ['а','б','в','г','д','е','ё','ж','з','и','й','к','л','м','н','о','п','р','с','т','у','ф','х','ц','ч','ш','щ','ъ','ы','ь','э','ю','я',
+            'А','Б','В','Г','Д','Е','Ё','Ж','З','И','Й','К','Л','М','Н','О','П','Р','С','Т','У','Ф','Х','Ц','Ч','Ш','Щ','Ъ','Ы','Ь','Э','Ю','Я'];
+    $lat = ['a','b','v','g','d','e','yo','zh','z','i','y','k','l','m','n','o','p','r','s','t','u','f','kh','ts','ch','sh','sch','','y','','e','yu','ya',
+            'A','B','V','G','D','E','Yo','Zh','Z','I','Y','K','L','M','N','O','P','R','S','T','U','F','Kh','Ts','Ch','Sh','Sch','','Y','','E','Yu','Ya'];
+    
+    // Вместо mb_strtolower используем обычную, она работает с ASCII
+    return str_replace($cyr, $lat, $text);
 }
-
 function generateRandomPassword($length = 10) {
     $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%';
     return substr(str_shuffle($chars), 0, $length);
